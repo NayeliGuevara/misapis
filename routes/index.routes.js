f269fa6380897a19b35d5ef40f72ac4a27ea6a4e
pervizurita <100954128+PervisZurita@users.noreply.github.com>
2024-11-06 23:31:01 -0500

    primera

---
+++ routes/index.routes.js
@@ -0,0 +1,9 @@
+import ejemplo from './ejemplo.routes.js';
+import { Router } from 'express';
+
+
+const indexRoutes = Router();
+
+indexRoutes.use('/ejemplo', ejemplo);
+
+export default indexRoutes;
